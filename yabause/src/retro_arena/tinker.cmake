41763e4376d919fd78f6a7b8e84624f20540dc14
slaminger <31171364+slaminger@users.noreply.github.com>
2021-01-11 16:37:29 -0600

    Add files via upload

---
+++ yabause/src/retro_arena/tinker.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set (USE_EGL True)
+
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_POSIX_C_SOURCE=199309L" )
+add_definitions( -D__TINKER__ )
+add_definitions( -D__RETRO_PIE__ )
