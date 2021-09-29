7ad03daa1cd73ac81c624061668e0d7539db1680
slaminger <31171364+slaminger@users.noreply.github.com>
2021-09-28 20:20:59 -0500

    Update and rename GoSuper.cmake to goadvance.cmake

--- yabause/src/retro_arena/goadvance.cmake
+++ yabause/src/retro_arena/goadvance.cmake
@@ -4,5 +4,5 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set (USE_EGL True)
 
 set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_POSIX_C_SOURCE=199309L" )
-add_definitions( -D__rpi4__ )
+add_definitions( -D__goadvance__ )
 add_definitions( -D__RETRO_PIE__ )
